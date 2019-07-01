9691864ed13e8a8b116fee7ec6cd6aec6cfcc0f3
Soubhagya <38796292+monetree@users.noreply.github.com>
2019-07-01 11:23:07 +0530

    fixed jump search

--- Algorithms/Searching/JumpSearch.js
+++ Algorithms/Searching/JumpSearch.js
@@ -3,20 +3,22 @@
 // search linearly by going left and right.
 
 
-arr = [ 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 ] 
-
 const JumpSearch = (arr, num) => {
     const length = arr.length;
     const eachBlock = Math.floor(length/4);
+    let found_big = false
     let index    = 0;
     while(index < length){
         let temp    = arr[index];
         if(num < temp){
             //search left
+            found_big=true
             index = index - 1;
         } else if(num === temp) {
             //matched return this
             return true
+        }else if((num > temp) && (found_big === true)) {
+            return false;
         } else {
             // jump eachBlock  
             index = index + eachBlock;
@@ -24,7 +26,7 @@ const JumpSearch = (arr, num) => {
     }
     return false;
 
-} // O(√4)
+} //O(n/4)
 
 console.log(JumpSearch(arr, 14))
 
